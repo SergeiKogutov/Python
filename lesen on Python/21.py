0ceda7943be19f76331fd52d11e5e94286deb45e
sergei kogutov <servik2003@gmail.com>
2023-02-16 15:53:02 +0700

    22

--- lesen on Python/21.py
+++ lesen on Python/21.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 # A = [1743, 1648, 1650, 1622, 1581, 1490]
@@ -43,3 +43,46 @@ def zd1():
     cx.get_xaxis().set_visible(False)
     cx.get_yaxis().set_ticklabels(['8', '6', '4', '2'])
     plt.show()
+
+def zd2():
+    # Задача 1. Проверьте, существует ли связь между данными, 
+    # приведёнными в таблице.Выполните задание с помощью 
+    # графика и библиотеки numpy.
+    x = [56, 37, 48, 45, 46, 43, 41, 45, 47, 48, 57, 63]
+    y = [66, 46, 46, 54, 57, 51, 52, 54, 57, 54, 68, 72]
+    z = [89, 67, 65, 77, 79, 68, 74, 75, 77, 77, 91, 96]
+
+    matrik = [x, y, z]
+    result = np.corrcoef(matrik)
+    print(result)
+    plt.plot(x, 'g')
+    plt.plot(y, 'r')
+    plt.plot(z, 'b')
+    plt.show()
+
+def zd3():
+    numbers = [np.random.randint(1,10) for i in range(5)]
+    num_av = np.mean(numbers)
+    print(numbers)
+    numbers = list(set(numbers))    
+    print(num_av)
+    x = int(input('введите число: '))
+    delta = [np.abs(number - x) for number in numbers]
+    min = np.argmin(delta)
+    print(numbers[min])
+
+def zd4():
+    size = (4, 4)
+    numbers = np.random.randint(1, 12, size)
+    print(numbers)
+    numbers_dict = {}
+    for row in numbers:
+        for num in row:
+            if num in numbers_dict.keys():
+                numbers_dict[num] += 1
+            else:
+                numbers_dict[num] = 1
+    print(numbers_dict)
+
+
+zd4()
